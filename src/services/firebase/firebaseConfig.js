5ca61bced0a022b71298fed90355a5dcf02322e4
Santiago Gimpelj <santigimpelj2002@gmail.com>
2024-02-05 19:33:10 -0300

    agrege .env

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDU5xoK5FiPiTAVY6ANS3iN2kQSvzU6iOg",
-  authDomain: "distribuidora-rpg.firebaseapp.com",
-  projectId: "distribuidora-rpg",
-  storageBucket: "distribuidora-rpg.appspot.com",
-  messagingSenderId: "64621315594",
-  appId: "1:64621315594:web:4edf8aae51c860bc6581f3"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
+
 
 
 const app = initializeApp(firebaseConfig);
